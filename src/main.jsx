c0094d8bc938cfdd40cd1afbc86d989714414e9b
ShayanBlaze <shayan.13.sharifi@gmail.com>
2026-02-22 18:07:40 +0330

    Temporarily disable checking for successful user authentication to access subsequent pages

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,14 @@ import "./assets/styles/index.css";
 import App from "./App.jsx";
 import { AuthProvider } from "./contexts/AuthContext";
 
-if (import.meta.env.DEV) {
-  const { DEV_TOKEN, DEV_USER } = await import("./dev/mock-auth.js");
+// if (import.meta.env.DEV) {
+//   const { DEV_TOKEN, DEV_USER } = await import("./dev/mock-auth.js");
 
-  if (!localStorage.getItem("token")) {
-    localStorage.setItem("token", DEV_TOKEN);
-    localStorage.setItem("user", JSON.stringify(DEV_USER));
-  }
-}
+//   if (!localStorage.getItem("token")) {
+//     localStorage.setItem("token", DEV_TOKEN);
+//     localStorage.setItem("user", JSON.stringify(DEV_USER));
+//   }
+// }
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
