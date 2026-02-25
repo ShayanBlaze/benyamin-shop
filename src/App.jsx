747183c6bcf4180d17962069da13860ce190ab32
Shayan Sharifi <132371339+ShayanBlaze@users.noreply.github.com>
2026-02-25 20:22:08 +0330

    feat: register dashboard routes with ProtectedRoute guard

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,38 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
+import DashboardLayout from "./layouts/DashboardLayout";
 import Home from "./pages/Home/Home";
 import Auth from "./pages/Auth/Auth";
+import ProtectedRoute from "./components/common/ProtectedRoute";
+import DashboardHome from "./pages/Dashboard/DashboardHome";
+import { Orders, Favorites, Address, Account } from "./pages/Dashboard/DashboardPlaceholders";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Public Routes */}
         <Route path="/auth" element={<Auth />} />
 
+        {/* Main Website Routes */}
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Home />} />
+        </Route>
+
+        {/* Protected Dashboard Routes */}
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <DashboardLayout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<DashboardHome />} />
+          <Route path="orders" element={<Orders />} />
+          <Route path="favorites" element={<Favorites />} />
+          <Route path="address" element={<Address />} />
+          <Route path="account" element={<Account />} />
         </Route>
       </Routes>
     </BrowserRouter>
